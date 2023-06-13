247b41566c5fe866c72bdc9b208a22a616fd4ddd
Jay Parisi <jay.parisi@coinbase.com>
2023-06-13 10:01:36 -0400

    added modal UI for staking

--- src/UI/WalletBalance.svelte
+++ src/UI/WalletBalance.svelte
@@ -1,10 +1,13 @@
 <script>
   import { getWalletBalance } from '../apis/Wallets';
   import { onMount } from 'svelte';
-  import { Button } from 'carbon-components-svelte';
+  import { Button, Modal } from 'carbon-components-svelte';
 
   export let walletId;
   let walletBalanceDetails;
+  let isModalOpen = false;
+  let modalState = 'preview';
+  let modalProps = {};
 
   onMount(async () => {
     walletBalanceDetails = await getWalletBalance(walletId);
@@ -15,11 +18,36 @@
   };
 
   const unstakeClicked = () => {
-    console.log('Unstake clicked');
+    isModalOpen = true;
+    modalProps = {
+      heading: `Unstake ${walletBalanceDetails.balance.amount}`,
+      type: 'unstake',
+      description: `Would you like to unstake your ${walletBalanceDetails.balance.bonded_amount} amount?`,
+    };
   };
 
   const restakeClicked = () => {
-    console.log('Restake clicked');
+    isModalOpen = true;
+    modalProps = {
+      heading: 'Restake Rewards',
+      type: 'restake',
+      description: `Would you like to restake your ${walletBalanceDetails.balance.pending_rewards_amount} rewards amount?`,
+    };
+  };
+
+  const executeRestake = () => {
+    console.log('Restake executed');
+    modalState = 'response';
+    modalProps = {
+      heading: 'Restake Rewards Response',
+      type: 'restake',
+      description: 'Status: Success',
+    };
+
+  };
+
+  const closeModal = () => {
+    isModalOpen = false;
   };
 </script>
 
@@ -30,13 +58,46 @@
   <h4>Unbonded Amount: {walletBalanceDetails.balance.unbonding_amount}</h4>
   <h4>Unvested Amount: {walletBalanceDetails.balance.unvested_amount}</h4>
   <h4>Rewards Amount: {walletBalanceDetails.balance.pending_rewards_amount}</h4>
-  <h4>Past Rewards Amount: {walletBalanceDetails.balance.past_rewards_amount}</h4><br>
+  <h4>Past Rewards Amount: {walletBalanceDetails.balance.past_rewards_amount}</h4>
 
   {#if walletBalanceDetails.balance.bonded_amount > 0}
     <Button size="small" on:click={unstakeClicked}>Unstake</Button>
-    <Button size="small" on:click={restakeClicked}>Restake Rewards</Button>
-  {:else if walletBalanceDetails.balance.bonded_amount === 0}
+    {#if walletBalanceDetails.balance.pending_rewards_amount > 0}
+      <Button size="small" on:click={restakeClicked}>Restake Rewards</Button>
+    {/if}
+  {:else}
     <Button size="small" on:click={stakeClicked}>Stake</Button>
+  {/if}
+
+  {#if modalState === 'preview'}
+    <Modal
+      bind:open={isModalOpen}
+      on:close={closeModal}
+      modalHeading={modalProps.heading}
+      primaryButtonText="Confirm {modalProps.type}"
+      secondaryButtonText="Cancel"
+      on:click:button--secondary={() => (isModalOpen = false)}
+      on:open
+      on:close
+      on:submit={executeRestake}
+    >
+      <p>{modalProps.description}</p>
+    </Modal>
+  {/if}
+
+  {#if modalState === 'response'}
+    <Modal
+      bind:open={isModalOpen}
+      on:close={closeModal}
+      primaryButtonText="Close"
+      modalHeading={modalProps.heading}
+      on:open
+      on:close
+      on:submit={closeModal}
+
+    >
+      <p>{modalProps.description}</p>
+    </Modal>
   {/if}
 
   <br /><br>
